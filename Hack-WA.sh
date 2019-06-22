5587c908de70af9a5673c5b3c3b25ab7336b2594
Ahmed0289 <52109314+Ahmed0289@users.noreply.github.com>
2019-06-23 00:17:35 +0700

    Hack-WA

---
+++ Hack-WA.sh
@@ -0,0 +1,125 @@
+#Gila_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo  $green
+echo "Loading.............";
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR GOBLOK.........................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................';
+sleep 0.1;
+clear
+echo  $red
+echo 'SABAR GOBLOK............................................';
+sleep 0.4;
+echo  $green
+echo "SABAR GOBLOK..................................";
+sleep 0.1;
+clear
+echo  $green
+echo  'Harap Sabar...............';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar...........................';
+sleep 0.1;
+echo  $green
+echo "Loading.........................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar....................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading GOBLOK................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar GOBLOK............................................';
+sleep 0.1;
+echo  $green
+echo "Loading.....................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar GOBLOK.............................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading........................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar GOBLOK........................';
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar ANJG...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR KONTOL................';
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR NGENTOD.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar BANGSD...................................................';
+sleep 0.4;
+echo  $green
+echo "NAH BERES ANJG....................................";
+sleep 0.1;
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    GUE GANTENG" | lolcat
+echo
+echo $red"##########################$green" SEGER? "$red###########################"
+echo $yellow
+figlet -f future "DAN LOE BURIQ"
+echo
+echo $red"##########################$green" BAU MEMEK "$red###########################"
+echo
